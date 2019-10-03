37ee3ba9015df850146ef39044c8323e7c833e7b
vbyrappa <vijay.byrappa@gmail.com>
2019-10-03 20:47:57 +0530

    Code being commited with a single class

---
+++ MavenJenkinsDemo/src/test/java/MavenProject/MavenJenkinsDemo/VerifyErrorMessage.java
@@ -0,0 +1,33 @@
+package MavenProject.MavenJenkinsDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class VerifyErrorMessage {
+	static WebDriver driver;
+public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		driver.get("https://facebook.com");
+		
+		driver.findElement(By.id("email")).sendKeys("vijay.byrappa@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("P@ssword123");
+		driver.findElement(By.id("loginbutton")).click();
+		
+		//Compare the Error message with the expected and Actual
+		String expectedMessage = "The email address that you've entered doesn't match any account. Sign up for an account.";
+		String actualMessage = driver.findElement(By.cssSelector("#globalContainer > div.uiContextualLayerPositioner._572t.uiLayer > div > div > div")).getText();
+		
+		if (expectedMessage.equals(actualMessage)){
+			System.out.println("The Error message is displaying properly : " + expectedMessage);
+		}
+		else
+		{
+			System.out.println("The Error message is mnot displaying the proper message : " + actualMessage);
+		}
+		driver.close();
+	}	
+}
